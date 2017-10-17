eab33923d6e773bd902fc07201f47690fd399f5a
weiyao86 <weiyaoisnull@gmail.com>
2017-10-17 18:01:07 +0800

    test

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-var extjsConfig = App.extjsConfig;
-var bootPage = extjsConfig.pages[extjsConfig.pageCode];
+// var extjsConfig = App.extjsConfig;
+// var bootPage = extjsConfig.pages[extjsConfig.pageCode];
+
+// Ext.application({
+// 	name: 'App',
+
+// 	extend: 'App.Application',
+
+// 	requires: [
+// 		'App.*',
+// 		'Ext.util.Common'
+// 	],
+
+// 	controllers: [bootPage.controller],
+
+// 	mainView: bootPage.viewport
+
+// });
+
 
 Ext.application({
 	name: 'App',
-
-	extend: 'App.Application',
-
+	extend: 'Ext.app.Application',
+	appFolder: 'app',
 	requires: [
 		'App.*',
 		'Ext.util.Common'
 	],
-
-	controllers: [bootPage.controller],
-
-	mainView: bootPage.viewport
-
+	paths: {
+		"App": App.globalConfig.appPath,
+		'Ext.ux': App.globalConfig.uxPath
+	},
+	controllers: [App.pageConfig.controller],
+	mainView: App.pageConfig.viewport
 });
--- app/view/account/userManage/Query.js
+++ app/view/account/userManage/Query.js
@@ -1,9 +1,33 @@
+Ext.define('App.form.SearchField', {
+	extend: 'Ext.form.field.Base',
+	alias: 'widget.searchField',
+	inputType: 'search',
+	searchUrl: 'http://www.baidu.com',
+	//fieldSubTpl: '<a>百度</a>',
+	initComponent: function() {
+		this.callParent(arguments);
+		this.on('specialkey', this.checkEnterKey, this);
+		this.on('click', this.clickKey, this);
+	},
+	checkEnterKey: function(field, e) {
+		var val = this.getValue();
+		if (e.getKey() == e.ENTER && !Ext.isEmpty(val)) {
+			var url = Ext.String.format(this.searchUrl, val);
+			window.open(url);
+		}
+	}
+});
+
+
 Ext.define('App.view.account.userManage.Query', {
 	extend: 'Ext.ux.component.filter.Query',
 	alias: 'widget.userquery',
 	requires: ['Ext.ux.component.combo.BaseCombo'],
 	items: [{
 		items: [{
+			xtype: 'searchField',
+			fieldLabel: '如果回车我就跳转'
+		}, {
 			fieldLabel: '用户名',
 			name: 'username'
 		}, {
